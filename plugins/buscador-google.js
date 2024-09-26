b8a7c709d56a7c8760b00c939dee3ab98938f6b3
Mickey Vip Pro <nexus.hosting.vip@gmail.com>
2024-09-26 04:36:27 -0500

    Add files via upload

--- plugins/buscador-google.js
+++ plugins/buscador-google.js
@@ -4,18 +4,18 @@ import axios from 'axios'
 let handler = async (m, { conn, command, args, usedPrefix }) => {
 const fetch = (await import('node-fetch')).default;
 const text = args.join` `
-if (!text) return conn.reply(m.chat, '🌸 Ingresa lo que deseas buscar en Google.', m, rcanal)
-conn.reply(m.chat, `🍭 Buscando Su Información...`, m, {
+if (!text) return conn.reply(m.chat, '🍟 Ingresa lo que deseas buscar en Google.', m, rcanal)
+conn.reply(m.chat, `🚩 Buscando Su Información...`, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
 title: packname,
 body: wm,
 previewType: 0, thumbnail: icons,
-sourceUrl: channel }}})
+sourceUrl: canal }}})
 const url = 'https://google.com/search?q=' + encodeURIComponent(text)
 google({'query': text}).then(res => {
-let teks = `🌸 *Resultado de* : ${text}\n\n`
+let teks = `🍟 *Resultado de* : ${text}\n\n`
 for (let g of res) {
-teks += `🍄 *Titulo ∙* ${g.title}\n🚩 *Info ∙* ${g.snippet}\n🔗 *Url ∙* ${g.link}\n\n`
+teks += `🐢 *Titulo ∙* ${g.title}\n🚩 *Info ∙* ${g.snippet}\n🔗 *Url ∙* ${g.link}\n\n`
 }
 conn.reply(m.chat, teks, m, rcanal)
 })
@@ -23,5 +23,6 @@ conn.reply(m.chat, teks, m, rcanal)
 handler.help = ['google <búsqueda>']
 handler.tags = ['buscador']
 handler.command = ['google']
-handler.register = true 
+handler.register = true
 export default handler
+
