22f6948819e1ffaf90fbf86dbc14ff053e2bee95
Mickey Vip Pro <nexus.hosting.vip@gmail.com>
2024-10-13 08:19:17 -0500

    Add files via upload

--- plugins/descargas-yt_play (1).js
+++ plugins/descargas-yt_play (1).js
@@ -1,180 +1,47 @@
-import fetch from 'node-fetch';
-import axios from 'axios';
-import { youtubedl, youtubedlv2 } from '@bochilteam/scraper';
-import fs from "fs";
-import yts from 'yt-search';
-import ytmp33 from '../lib/ytmp33.js';
-import ytmp44 from '../lib/ytmp44.js';
-const {proto, generateWAMessageFromContent, prepareWAMessageMedia, generateWAMessageContent, getDevice} = (await import("@whiskeysockets/baileys")).default
+import fg from 'api-dylux'
+import yts from 'yt-search'
+import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
+let limit = 350
+let handler = async (m, { conn, text, isPrems, isOwner, usedPrefix, command }) => {
+if (!m.quoted) return conn.reply(m.chat, '🚩 *Etiqueta el mensaje que contenga el resultado de Play*', m, rcanal)
+if (!m.quoted.text.includes("*乂  Y O U T U B E  -  P L A Y  乂*")) return conn.reply(m.chat, '🚩 *Etiqueta el mensaje que contenga el resultado de Play*', m, rcanal)
+if (!m.quoted.isBaileys) return conn.reply(m.chat, '🚩 Etiqueta el mensaje mío del resultado Play', m, rcanal)
+let urls = m.quoted.text.match(new RegExp(/(?:https?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch|v|embed|shorts)(?:\.php)?(?:\?.*v=|\/))([a-zA-Z0-9\_-]+)/, 'gi'))
+if (!urls) return m.reply('×')
+if (urls.length < text) return conn.reply(m.chat, '🚩 *No se encontraron resultados*', rcanal)
+let q = '128kbps'
+try {
+await m.react(rwait)
+const yt = await fg.yta(urls[0], q)
+let { title, dl_url, size } = yt 
 
-let limit1 = 100;
-let limit2 = 400;
-let limit_a1 = 50;
-let limit_a2 = 400;
+if (size.split('MB')[0] >= limit) return conn.reply(m.chat, `🚩 El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m, rcanal) 
 
-const handler = async (m, { conn, command, args, text, usedPrefix }) => {
-  if (!text) throw '*[❗] Ingresa un texto.*';
+conn.reply(m.chat, `🚀 Descargando El Audio...`, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
+title: packname,
+body: dev,
+previewType: 0, thumbnail: icons,
+sourceUrl: channel }}})
 
-  const yt_play = await search(args.join(' '));
-  let additionalText = '';
-  if (command === 'Audio') {
-    additionalText = 'audio';
-  } else if (command === 'Burbuja') {
-    additionalText = 'vídeo';
-  }
+await conn.sendMessage(m.chat, { audio: { url: dl_url }, fileName: title + '.mp3', mimetype: 'audio/mp4' }, { quoted: fkontak })
+await m.react(done)
+} catch {
+try {
+let yt = await fg.ytmp3(urls[0], q)
+let { title, size, dl_url } = yt
 
-  const texto1 = `*Título:* ${yt_play[0].title}\n*Subido hace:* ${yt_play[0].ago}\n*Duración:* ${secondString(yt_play[0].duration.seconds)}\n*Vistas:* ${MilesNumber(yt_play[0].views)}\n*Autor:* ${yt_play[0].author.name}\n*ID del video:* ${yt_play[0].videoId}\n*Tipo:* ${yt_play[0].type}\n*URL:* ${yt_play[0].url}\n*URL del autor:* ${yt_play[0].author.url}\n\n> *Se está enviando el ${additionalText}...*`.trim();
+if (size.split('MB')[0] >= limit) return conn.reply(m.chat, `🚩 El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m, rcanal) 
 
-  await conn.sendMessage(m.chat, { image: { url: yt_play[0].thumbnail }, caption: texto1 }, { quoted: m });
-
-  if (command === 'Audio') {
-    try {
-      const { status, resultados, error } = await ytmp33(yt_play[0].url);
-      if (!status) throw new Error(error);
-
-      const ttl = resultados.titulo;
-      const buff_aud = await getBuffer(resultados.descargar);
-      const fileSizeInBytes = buff_aud.byteLength;
-      const fileSizeInKB = fileSizeInBytes / 1024;
-      const fileSizeInMB = fileSizeInKB / 1024;
-      const size = fileSizeInMB.toFixed(2);
-
-      if (size >= limit_a2) {
-        await conn.sendMessage(m.chat, { text: `*[❗] El archivo es muy grande para enviarlo directamente. Puedes descargarlo desde el siguiente enlace:* _${resultados.descargar}_` }, { quoted: m });
-        return;
-      }
-      if (size >= limit_a1 && size <= limit_a2) {
-        await conn.sendMessage(m.chat, { document: buff_aud, mimetype: 'audio/mpeg', fileName: ttl + `.mp3` }, { quoted: m });
-        return;
-      } else {
-        await conn.sendMessage(m.chat, { audio: buff_aud, mimetype: 'audio/mpeg', fileName: ttl + `.mp3` }, { quoted: m });
-        return;
-      }
-    } catch (error) {
-      console.log('Fallo el 1: ' + error);
-      try {
-        const audio = `https://api.cafirexos.com/api/v1/ytmp3?url=${yt_play[0].url}&apikey=BrunoSobrino`;
-        const ttl = await yt_play[0].title;
-        const buff_aud = await getBuffer(audio);
-        const fileSizeInBytes = buff_aud.byteLength;
-        const fileSizeInKB = fileSizeInBytes / 1024;
-        const fileSizeInMB = fileSizeInKB / 1024;
-        const size = fileSizeInMB.toFixed(2);
-
-        if (size >= limit_a2) {
-          await conn.sendMessage(m.chat, { text: `*[❗] El archivo es muy grande para enviarlo directamente. Puedes descargarlo desde el siguiente enlace:* _${audio}_` }, { quoted: m });
-          return;
-        }
-        if (size >= limit_a1 && size <= limit_a2) {
-          await conn.sendMessage(m.chat, { document: buff_aud, mimetype: 'audio/mpeg', fileName: ttl + `.mp3` }, { quoted: m });
-          return;
-        } else {
-          await conn.sendMessage(m.chat, { audio: buff_aud, mimetype: 'audio/mpeg', fileName: ttl + `.mp3` }, { quoted: m });
-          return;
-        }
-      } catch {
-        throw '*[❗] Error al procesar la solicitud.*';
-      }
-    }
-  }
-
-  if (command === 'Burbuja') {
-    try {
-      const { status, resultados, error } = await ytmp44(yt_play[0].url);
-      if (!status) throw new Error(error);
-
-      const ttl2 = resultados.titulo;
-      const buff_vid = await getBuffer(resultados.descargar);
-      const fileSizeInBytes2 = buff_vid.byteLength;
-      const fileSizeInKB2 = fileSizeInBytes2 / 1024;
-      const fileSizeInMB2 = fileSizeInKB2 / 1024;
-      const size2 = fileSizeInMB2.toFixed(2);
-
-      if (size2 >= limit2) {
-        await conn.sendMessage(m.chat, { text: `*[❗] El archivo es muy grande para enviarlo directamente. Puedes descargarlo desde el siguiente enlace:* _${resultados.descargar}_` }, { quoted: m });
-        return;
-      }
-      if (size2 >= limit1 && size2 <= limit2) {
-        await conn.sendMessage(m.chat, { document: buff_vid, mimetype: 'video/mp4', fileName: ttl2 + `.mp4` }, { quoted: m });
-        return;
-      } else {
-        const ppt = await generateWAMessageContent({ video: buff_vid }, { upload: conn.waUploadToServer });
-        const ptv = generateWAMessageFromContent(m.chat, proto.Message.fromObject({ "ptvMessage": ppt.videoMessage, caption: `Listo`, fileLength: 9999999999 }), { quoted: m });
-        await conn.relayMessage(m.chat, ptv.message, { messageId: ptv.key.id });
-        return;
-      }
-    } catch (error) {
-      console.log('Fallo el 1: ' + error);
-      try {
-        const video = `https://api.cafirexos.com/api/v1/ytmp4?url=${yt_play[0].url}&apikey=BrunoSobrino`;
-        const ttl2 = await yt_play[0].title;
-        const buff_vid = await getBuffer(video);
-        const fileSizeInBytes2 = buff_vid.byteLength;
-        const fileSizeInKB2 = fileSizeInBytes2 / 1024;
-        const fileSizeInMB2 = fileSizeInKB2 / 1024;
-        const size2 = fileSizeInMB2.toFixed(2);
-
-        if (size2 >= limit2) {
-          await conn.sendMessage(m.chat, { text: `*[❗] El archivo es muy grande para enviarlo directamente. Puedes descargarlo desde el siguiente enlace:* _${video}_` }, { quoted: m });
-          return;
-        }
-        if (size2 >= limit1 && size2 <= limit2) {
-          await conn.sendMessage(m.chat, { document: buff_vid, mimetype: 'video/mp4', fileName: ttl2 + `.mp4` }, { quoted: m });
-          return;
-        } else {
-          const ppt = await generateWAMessageContent({ video: buff_vid }, { upload: conn.waUploadToServer });
-          const ptv = generateWAMessageFromContent(m.chat, proto.Message.fromObject({ "ptvMessage": ppt.videoMessage, caption: `Listo`, fileLength: 9999999999 }), { quoted: m });
-          await conn.relayMessage(m.chat, ptv.message, { messageId: ptv.key.id });
-          return;
-        }
-      } catch {
-        throw '*[❗] Error al procesar la solicitud.*';
-      }
-    }
-  }
-};
-
-handler.command = /^(Audio|Burbuja)$/i;
-export default handler;
-
-async function search(query, options = {}) {
-  const search = await yts.search({query, hl: 'es', gl: 'ES', ...options});
-  return search.videos;
-}
-
-function MilesNumber(number) {
-  const exp = /(\d)(?=(\d{3})+(?!\d))/g;
-  const rep = '$1.';
-  const arr = number.toString().split('.');
-  arr[0] = arr[0].replace(exp, rep);
-  return arr[1] ? arr.join('.') : arr[0];
-}
-
-function secondString(seconds) {
-  seconds = Number(seconds);
-  const d = Math.floor(seconds / (3600 * 24));
-  const h = Math.floor((seconds % (3600 * 24)) / 3600);
-  const m = Math.floor((seconds % 3600) / 60);
-  const s = Math.floor(seconds % 60);
-  const dDisplay = d > 0 ? d + (d == 1 ? 'd ' : 'd ') : '';
-  const hDisplay = h > 0 ? h + (h == 1 ? 'h ' : 'h ') : '';
-  const mDisplay = m > 0 ? m + (m == 1 ? 'm ' : 'm ') : '';
-  const sDisplay = s > 0 ? s + (s == 1 ? 's' : 's') : '';
-  return dDisplay + hDisplay + mDisplay + sDisplay;
-}
-
-function bytesToSize(bytes) {
-  return new Promise((resolve, reject) => {
-    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-    if (bytes === 0) return 'n/a';
-    const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10);
-    if (i === 0) resolve(`${bytes} ${sizes[i]}`);
-    resolve(`${(bytes / (1024 ** i)).toFixed(1)} ${sizes[i]}`);
-  });
-}
-
-const getBuffer = async (url, options) => {
-    options ? options : {};
-    const res = await axios({method: 'get', url, headers: {'DNT': 1, 'Upgrade-Insecure-Request': 1,}, ...options, responseType: 'arraybuffer'});
-    return res.data;
-};
+await conn.sendMessage(m.chat, { audio: { url: dl_url }, fileName: title + '.mp3', mimetype: 'audio/mp4' }, { quoted: fkontak })
+await m.react(done)
+} catch {
+await m.react(error)
+await m.reply(`✘ *Ocurrío un error*`)
+}}}
+handler.help = ['audio']
+handler.tags = ['descargas']
+handler.customPrefix = /^(2|audio|Audio)/
+handler.command = new RegExp
+handler.register = true
+export default handler
